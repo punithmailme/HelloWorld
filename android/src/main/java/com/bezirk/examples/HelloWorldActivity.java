3a8027e4f88e3b72d17ad02cdbd78ffa86119a3c
Michael Maass <mam1pi@bosch.com>
2016-06-06 15:08:58 -0400

    Updated API to alpha+010

--- android/src/main/java/com/bezirk/examples/HelloWorldActivity.java
+++ android/src/main/java/com/bezirk/examples/HelloWorldActivity.java
@@ -9,9 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.bezirk.middleware.Bezirk;
-import com.bezirk.middleware.addressing.Address;
-import com.bezirk.middleware.addressing.Location;
-import com.bezirk.middleware.addressing.ZirkId;
 import com.bezirk.middleware.messages.Event;
 import com.bezirk.middleware.messages.Message;
 import com.bezirk.middleware.proxy.android.Factory;
@@ -19,7 +16,6 @@ import com.bezirk.middleware.proxy.android.Factory;
 public class HelloWorldActivity extends AppCompatActivity {
 
     private Bezirk bezirk;
-    private ZirkId myId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +24,7 @@ public class HelloWorldActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        bezirk = Factory.getInstance(this);
-        myId = bezirk.registerZirk("HelloWorldAndroidService");
+        bezirk = Factory.registerZirk(this, "HelloWorldAndroidService");
         sayHello();
     }
 
@@ -57,14 +52,11 @@ public class HelloWorldActivity extends AppCompatActivity {
 
     public void sayHello() {
         // Steps to publish an even over UhU:
-        // 1. set the targeted address
-        Address target = new Address(new Location(null));        // local only (no pipes) with no constraints on location: will reach all services in the spheres HelloWorld is a member of
-
-        // 2. set the event to be published
+        // 1. set the event to be published
         Event hello = new Event(Message.Flag.NOTICE, "Hello World");
 
-        // 3. publish "hello world" to all in the target address
-        bezirk.sendEvent(myId, target, hello);
+        // 2. publish "hello world" to all in the target address
+        bezirk.sendEvent(hello);
 
         // display the event that was just published
         //System.out.println("Published: " + hello.serialize());
--- java/src/main/java/com/bezirk/examples/HelloWorld.java
+++ java/src/main/java/com/bezirk/examples/HelloWorld.java
@@ -1,9 +1,6 @@
 package com.bezirk.examples;
 
 import com.bezirk.middleware.Bezirk;
-import com.bezirk.middleware.addressing.Address;
-import com.bezirk.middleware.addressing.Location;
-import com.bezirk.middleware.addressing.ZirkId;
 import com.bezirk.middleware.messages.Event;
 import com.bezirk.middleware.messages.Message.Flag;
 import com.bezirk.middleware.proxy.Factory;
@@ -12,31 +9,26 @@ import javax.swing.JOptionPane;
 
 public class HelloWorld {
     private final Bezirk bezirk;
-    private final ZirkId myId;
 
     public HelloWorld() {
         // set up sending messages over bezirk
-        bezirk = Factory.getInstance();
-        myId = bezirk.registerZirk(HelloWorld.class.getSimpleName());
-    }
-
-    public void sayHello() {
-        // Steps to publish an event over bezirk
-        // 1. set the targeted address
-        Address target = new Address(new Location(null));        // local only (no pipes) with no constraints on location: will reach all services in the spheres HelloWorld is a member of
-
-        // 2. set the event to be published
-        Event hello = new Event(Flag.NOTICE, "Hello World");
-
-        // 3. publish "hello world" to all in the target address
-        bezirk.sendEvent(myId, target, hello);
-
-        // display the event that was just published
-        JOptionPane.showMessageDialog(null, "HelloWorld successful!!!\nPublished Message: " + hello.toJson());
+        bezirk = Factory.registerZirk(HelloWorld.class.getSimpleName());
     }
 
     public static void main(String[] args) {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.sayHello();
+    }
+
+    public void sayHello() {
+        // Steps to publish an event over bezirk
+        // 1. set the event to be published
+        Event hello = new Event(Flag.NOTICE, "Hello World");
+
+        // 2. publish "hello world" to all in the target address
+        bezirk.sendEvent(hello);
+
+        // display the event that was just published
+        JOptionPane.showMessageDialog(null, "HelloWorld successful!!!\nPublished Message: " + hello.toJson());
     }
 }
